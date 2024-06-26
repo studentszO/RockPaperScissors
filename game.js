4c61c7b91fc45748c3f93ca63195caea8d9ae817
studentszO <169681673+studentszO@users.noreply.github.com>
2024-06-26 16:13:09 +0200

    ADD a loop to play for 5 rounds

--- game.js
+++ game.js
@@ -1,24 +1,26 @@
 // CREATE function getComputerChoice
 function getComputerChoice() {
-// GET a random number with Math.random
-let random_number = Math.random();
-// IF random number is inferior to .33
-if (random_number <= 0.33)
-// return string Rock
-    return "rock"
-// ELSEIF random number superior to .66
-else if (random_number >= 0.66)
-// return string Scissors 
-    return "scissors"
-// ELSE return string paper
-else
-    return "paper"
+    // GET a random number with Math.random
+    let random_number = Math.random();
+
+    // IF random number is inferior to .33
+    if (random_number <= 0.33)
+    // return string Rock
+        return "rock"
+    // ELSEIF random number superior to .66
+    else if (random_number >= 0.66)
+    // return string Scissors 
+        return "scissors"
+    // ELSE return string paper
+    else
+        return "paper"
 }
 
 // CREATE function getHumanChoice
 function getHumanChoice() {
     // GET an input from the user
     let choice = prompt("What choice do you want to make ?").toLowerCase();
+
     // IF choice equals one of rock, paper or scissors
     if (choice === "rock" || choice === "paper" || choice === "scissors")
         // return user's choice
@@ -27,19 +29,6 @@ function getHumanChoice() {
     else
         return alert("WRONG! Your choice must be rock, paper or scissors")
 }
-
-
-
-// CREATE a VARIABLE humanScore & INITIALISE it to 0
-let humanScore = 0;
-// CREATE a VARIABLE computerScore & INITIALISE it to 0
-let computerScore = 0;
-
-// CREATE two variables to get each choice
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
 
 // CREATE function playGame
 function playGame() {
@@ -51,37 +40,60 @@ function playGame() {
     // CREATE function playRound with two parameters
     function playRound(humanChoice, computerChoice) {
         // IF humanChoice is the same as computerChoice
-        if (humanChoice === computerChoice)
+        if (humanChoice === computerChoice) {
             // RETURN log: It's a draw.
             return console.log("It's a draw!")
+        }
         // IF humanChoice != computerChoice
-        else 
-        // IF iterations for every choice with console.logs
+        else {
+            // IF iterations for every choice with console.logs
             if (humanChoice === "rock" && computerChoice === "paper") {
-                console.log("YOU LOSE! Paper beats Rock.")
-                computerScore++;
-            }
-            if (humanChoice === "rock" && computerChoice === "scissors") {
-                console.log("YOU WIN! Rock beats Scissors.")
-                humanScore++;
-            }
-            if (humanChoice === "scissors" && computerChoice === "rock") {
-                console.log("YOU LOSE! Rock beats Scissors.")
-                computerScore++;
-            }
-            if (humanChoice === "scissors" && computerChoice === "paper") {
-                console.log("YOU WIN! Scissors beats Paper.")
-                humanScore++;
-            }
-            if (humanChoice === "paper" && computerChoice === "rock") {
-                console.log("YOU WIN! Paper beats Rock.")
-                humanScore++;
-            }
-            if (humanChoice === "paper" && computerChoice == "scissors") {
-                console.log("YOU LOSE! Scissors beats Paper")
-                computerScore++;
-            }
+                    console.log("YOU LOSE! Paper beats Rock.")
+                    return computerScore++;
+                }
+                if (humanChoice === "rock" && computerChoice === "scissors") {
+                    console.log("YOU WIN! Rock beats Scissors.")
+                    return humanScore++;
+                }
+                if (humanChoice === "scissors" && computerChoice === "rock") {
+                    console.log("YOU LOSE! Rock beats Scissors.")
+                    return computerScore++;
+                }
+                if (humanChoice === "scissors" && computerChoice === "paper") {
+                    console.log("YOU WIN! Scissors beats Paper.")
+                    return humanScore++;
+                }
+                if (humanChoice === "paper" && computerChoice === "rock") {
+                    console.log("YOU WIN! Paper beats Rock.")
+                    return humanScore++;
+                }
+                if (humanChoice === "paper" && computerChoice == "scissors") {
+                    console.log("YOU LOSE! Scissors beats Paper")
+                    return computerScore++;
+                }
+        }
+    }
+
+    // Loop 5 times the playGame to play for 5 rounds
+    for (round = 0; round < 5; round++) {
+        humanSelection = getHumanChoice();
+        computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log(humanScore, computerScore, humanSelection, computerSelection)
+    }
+
+    if (humanScore > computerScore) {
+        return console.log("YOU WIN!")
+    }
+    else {
+        return console.log("YOU LOSE!")
     }
 }
 
-console.log(humanScore, computerScore, humanSelection, computerSelection)
+// CREATE two variables to get each choice
+let humanSelection;
+let computerSelection;
+
+playGame();
+
+// console.log(humanScore, computerScore, humanSelection, computerSelection)
